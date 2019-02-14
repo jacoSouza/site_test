8776722805c0ffc7cca82513dc39884aeddbd078
jacoSouza <jacozica1@gmail.com>
2019-02-14 18:55:40 -0200

    Primeira versão do site

---
+++ Noticia.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+                             <meta charset="UTF-8">
+
+    
+		
+		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		<link type="text/css" rel="stylesheet" href="estilo/estilo.css"/>
+	
+	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+                                 <title>Noticia</title>
+</head>
+<body>
+
+<header>				
+		<nav class="nav-extended">
+			<div class="nav-wrapper">
+				<a href="#" class="brand-logo">Logo</a>
+				<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+						<li><a href="sass.html">suporte</a></li>
+						<li><a href="badges.html">estatos</a></li>
+						<li><a href="collapsible.html">login</a></li>
+					</ul>
+			</div>
+			<div class="nav-content">
+				<ul class="tabs tabs-transparent">
+					<li class="tab"><a href="index.php">Home</a></li>
+					<li class="tab"><a class="active" href="Noticia.php">Noticia</a></li>
+					<li class="tab disabled"><a href="mercado.php">Mercado</a></li>
+					<li class="tab"><a href="compra.php">Compras</a></li>
+				</ul>
+			</div>
+			</nav>
+
+				<ul class="sidenav" id="mobile-demo">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="badges.html">Components</a></li>
+					<li><a href="collapsible.html">JavaScript</a></li>
+				</ul>
+
+				
+	
+	</header>        
+
+
+
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+         <script src="js/jvsc.js"></script> 
+
+</body>
+
+
+
+
+<footer>
+
+<div class="nav-content" id="foot">
+    <ul class="tabs tabs-transparent">
+        <li class="tab"><a href="#test1">fecebook</a></li>
+        <li class="tab"><a class="active" href="#test2">comenta</a></li>
+        <li class="tab disabled"><a href="#test3">twitter</a></li>
+    </ul>
+</div>
+
+</footer>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+							<meta charset="UTF-8">
+
+		
+		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		<link type="text/css" rel="stylesheet" href="estilo/estilo.css"/>
+	
+	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		
+		
+		
+							<title>HOME</title>
+</head>
+<body>
+
+	<header>				
+		<nav class="nav-extended">
+			<div class="nav-wrapper">
+				<a href="#" class="brand-logo">Logo</a>
+				<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+						<li><a href="sass.html">suporte</a></li>
+						<li><a href="badges.html">estatos</a></li>
+						<li><a href="collapsible.html">login</a></li>
+					</ul>
+			</div>
+			<div class="nav-content">
+				<ul class="tabs tabs-transparent">
+					<li class="tab"><a href="index.php">Home</a></li>
+					<li class="tab"><a class="active" href="Noticia.php">Noticia</a></li>
+					<li class="tab disabled"><a href="mercado.php">Mercado</a></li>
+					<li class="tab"><a href="compra.php">Compras</a></li>
+				</ul>
+			</div>
+			</nav>
+
+				<ul class="sidenav" id="mobile-demo">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="badges.html">Components</a></li>
+					<li><a href="collapsible.html">JavaScript</a></li>
+				</ul>
+
+				
+	
+	</header>
+
+	<div class="carousel" id="carousel">
+    <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
+    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
+    <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
+    <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
+    <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+	 </div>
+
+	 <div class="conteine">	
+					<!-- card 1 para conteudo -->
+		<div class="card" id="card1">
+		<div class="card-content">
+		<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+		</div>
+		<div class="card-tabs">
+		<ul class="tabs tabs-fixed-width">
+			<li class="tab"><a href="#test4">Test 1</a></li>
+			<li class="tab"><a class="active" href="#test5">Test 2</a></li>
+			<li class="tab"><a href="#test6">Test 3</a></li>
+		</ul>
+		</div>
+		<div class="card-content grey lighten-2">
+		<div id="test4">Test 1</div>
+		<div id="test5">Test 2</div>
+		<div id="test6">Test 3</div>
+		</div>
+		</div>
+		
+				<!--card 2 para conteudo -->
+		<div class="card" id="card2">
+		<div class="card-content">
+		<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+		</div>
+		<div class="card-tabs">
+		<ul class="tabs tabs-fixed-width">
+			<li class="tab"><a href="#test4">Test 1</a></li>
+			<li class="tab"><a class="active" href="#test5">Test 2</a></li>
+			<li class="tab"><a href="#test6">Test 3</a></li>
+		</ul>
+		</div>
+		<div class="card-content grey lighten-2">
+		<div id="test4">Test 1</div>
+		<div id="test5">Test 2</div>
+		<div id="test6">Test 3</div>
+		</div>
+		</div>
+
+				<!--card 3 para conteudo-->
+		<div class="card" id="card3">
+		<div class="card-content">
+		<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+		</div>
+		<div class="card-tabs">
+		<ul class="tabs tabs-fixed-width">
+			<li class="tab"><a href="#test4">Test 1</a></li>
+			<li class="tab"><a class="active" href="#test5">Test 2</a></li>
+			<li class="tab"><a href="#test6">Test 3</a></li>
+		</ul>
+		</div>
+		<div class="card-content grey lighten-2">
+		<div id="test4">Test 1</div>
+		<div id="test5">Test 2</div>
+		<div id="test6">Test 3</div>
+		</div>
+		</div>
+
+  </div> 
+	 <div class="comteudo">
+		<h1 id="a">ad</h1>
+	 </div>
+
+
+
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<script src="js/jvsc.js"></script> 
+</body>
+
+	
+
+<footer>
+
+	<div class="nav-content" id="foot">
+		<ul class="tabs tabs-transparent">
+			<li class="tab"><a href="#test1">fecebook</a></li>
+			<li class="tab"><a class="active" href="#test2">comenta</a></li>
+			<li class="tab disabled"><a href="#test3">twitter</a></li>
+		</ul>
+	</div>
+	
+</footer>
+
+
+
+</html>
